41d03ef3c017c0016186105a0bb99ad12b9cfe48
Rebecca Denham-Jones <rdenhamjones@gmail.com>
2025-06-21 16:28:36 +0100

    Pop-up content

--- script.js
+++ script.js
@@ -16,61 +16,67 @@ const projectDetails = {
     project1: {
       title: "Presenting",
       context: [
-        "Point 1 about context",
-        "Point 2 about context",
+        "Overview: I led a project from discovery through to delivery, focused on improving a highly manual and time-critical red business process used by our operations team. This process was client-facing and had to be completed on the first working day of each month, meaning any delays could have commercial consequences.",
       ],
       process: [
-        "Step 1 in the process",
-        "Step 2 in the process",
+        "My Role: I led the inital discovery, deleving into the pain points of our operations team and writing a product brief defining the problem. I scoped out initial prototyping with a senior engineer, building technical domain knowledge. Next, I led the wider engineering team through the build, breaking down complex problems into digestible items of work. ",
       ],
       outcome: [
-        "Outcome 1",
-        "Outcome 2",
+        "Outcome: We delivered the automation which removed a red operational processes, directly contributing to our divisions OKRs. It saved around 10 hours of manual work on each first of the month.",
       ],
       keySkills: [
-        "Skill 1",
-        "Skill 2",
+        "Key Skills: Product management, analytical and creative thinking, technical knowledge, collaboration, leadership.",
       ]
     },
 
     project2: {
         title: "Discovery", 
         context: [
-            "Overview:",
-            "The company aimed to improve the onboarding experience for new users, which was identified as a key factor in increasing user retention and satisfaction.",
-            "Point 2 about context",
+            "Overview: I led the discovery of a new interactive Savings tool for our clients. Senior stakeholders had shown an interest in this tool as our competitor's had a strong offering.",
           ],
           process: [
-            "My Role:",
-            "I led the product discovery phase, collaborating with UX designers and engineers to identify pain points through user interviews and data analysis.", 
-            "I helped define the MVP, prioritized features based on user impact and feasibility, and assisted in sprint planning.",
-            "Conducted 15+ user interviews and analyzed onboarding drop-off metrics. Created user journey maps and identified friction points.",
-            "Defined and prioritized feature backlog with stakeholders.",
-            "Coordinated with design and engineering teams through Agile ceremonies.",
-            "Monitored launch impact through usage analytics and user feedback.",
+            "My Role: I guided strategic direction and decision-making. This involved collaboring with a diverse range of teams, including individuals from user-centred design, marketing, compliance, product management, and senior stakeholders.  I supported UCD through prototyping and client interviews, ensuring clients needs were met while staying aligned with industry regulations. Additionally I managed the expectations of stakeholders throughout.",
           ],
           outcome: [
-            "Outcomes:",
-            "Reduced onboarding drop-off rate by 18% within three months post-launch.",
-            "Increased new user activation by 25%.",
-            "Received positive feedback from users citing clarity and ease of use",
+            "Outcome: Senior stakeholders were impressed with the designs and far exceeded their expectations of what was possible. This was taken into future roadmap priortisation.",
 
           ],
           keySkills: [
-            "Key Skills:",
-            "Stakeholder management",
-            "Communication",
-            "Cross-functional collaboration",
+            "Key Skills: Stakeholder management, communication, cross-functional collaboration, leadership.",
           ]
     },
 
     project3: {
         title: "Competitor Analysis", 
-        description: "this is a description of competitor analysis"
+        
+        context: [
+        "Overview: I provided weekly comepetitor reports to the wider business to 150+ people across the wider business including c-level stakeholders.",
+        ],
+        process: [
+        "My Role: I followed key financial news reporting on competitor or industry moves that were both interesting and relevant to Hargreaves Lansdown. This allowed me to build an understanding of all our services and our competitor landscape. Additionally I built strong commericial awareness. ",
+        ],
+        outcome: [
+        "Outcome: This was extremely positively received, with many senior stakeholders commenting on the usefulness and their interest in the weekly reports. The report became more wide spread throughout the year reaching new areas of the business.",
+        ],
+        keySkills: [
+        "Key Skills: Reporting, commericial awareness, business knowledge.",
+        ]
+    
     },
     project4: {
-        title: "Product", 
-        description: "this is a description of product learning"
+      title: "Presenting",
+      context: [
+        "Overview: I recognised presenting was a skill I needed to improve.",
+      ],
+      process: [
+        "My Role: I took steps to proactively seek opportunities to public speak. I started off small but gradually presented to larger audiences as I built my confidence. I actively sought feedback and acted upon it each time. ",
+      ],
+      outcome: [
+        "Outcome: Over my placement year I have presented to varying audiences, reaching c-level stakeholders and audiences of 80+ people",
+      ],
+      keySkills: [
+        "Key Skills: Public speaking and confidence.",
+      ]
     },
 };
 
@@ -78,8 +84,6 @@ const projectDetails = {
 document.querySelectorAll('.view-more').forEach(button => {
     button.addEventListener('click', function(e) {
         const project = e.target.getAttribute('data-project');
-        // popupTitle.innerText = projectDetails[project].title;
-        // popupDescription.innerText = projectDetails[project].description;
         const details = projectDetails[project];
         console.log('Clicked project:', project);
         console.log('Project details:', details);
@@ -98,32 +102,6 @@ document.querySelectorAll('.view-more').forEach(button => {
 closeButton.addEventListener('click', function() {
     popup.style.display = 'none'; // Hide the popup
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
